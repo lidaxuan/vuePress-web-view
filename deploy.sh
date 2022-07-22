a299fa83bbf7ab9a1f6c831c1ead0dc8a3343743
lijixuan <15201002062@163.com>
2022-07-22 20:28:03 +0800

    feat: 修改

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+###
+ # @Description: 
+ # @Author: 李大玄
+ # @Date: 2022-07-22 14:55:46
+ # @FilePath: /vuePress-web-view/deploy.sh
+ # @LastEditors: 李大玄
+ # @LastEditTime: 2022-07-22 14:55:47
+### 
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
--- docs/.vuepress/readFileList.js
+++ docs/.vuepress/readFileList.js
@@ -8,49 +8,49 @@
 /**
  *  读取所有md文件数据
  */
- const { log } = require('console');
+
 const fs = require('fs'); // 文件模块
- const path = require('path'); // 路径模块
- const docsRoot = path.join(__dirname, '..', '..', 'docs'); // docs文件路径
- 
- function readFileList(dir = docsRoot, filesList = []) {
-   const files = fs.readdirSync(dir);
-   files.forEach((item, index) => {
-     let filePath = path.join(dir, item);
-     const stat = fs.statSync(filePath);
-     if (stat.isDirectory() && item !== '.vuepress') {
-       readFileList(path.join(dir, item), filesList); //递归读取文件
-     } else {
-       if (path.basename(dir) !== 'docs') { // 过滤docs目录级下的文件
- 
-         const fileNameArr = path.basename(filePath).split('.')
-         console.log('fileNameArr', fileNameArr);
-         let name = null,
-           type = null;
-         if (fileNameArr.length === 2) { // 没有序号的文件
-           name = fileNameArr[0]
-           type = fileNameArr[1]
-         } else if (fileNameArr.length === 3) { // 有序号的文件
-           name = fileNameArr[1]
-           type = fileNameArr[2]
-         } else { // 超过两个‘.’的
+const path = require('path'); // 路径模块
+const docsRoot = path.join(__dirname, '..', '..', 'docs'); // docs文件路径
+
+function readFileList(dir = docsRoot, filesList = []) {
+  const files = fs.readdirSync(dir);
+  files.forEach((item, index) => {
+    let filePath = path.join(dir, item);
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory() && item !== '.vuepress') {
+      readFileList(path.join(dir, item), filesList); //递归读取文件
+    } else {
+      if (path.basename(dir) !== 'docs') { // 过滤docs目录级下的文件
+
+        const fileNameArr = path.basename(filePath).split('.')
+        console.log('fileNameArr', fileNameArr);
+        let name = null,
+          type = null;
+        if (fileNameArr.length === 2) { // 没有序号的文件
+          name = fileNameArr[0]
+          type = fileNameArr[1]
+        } else if (fileNameArr.length === 3) { // 有序号的文件
+          name = fileNameArr[1]
+          type = fileNameArr[2]
+        } else { // 超过两个‘.’的
           //  console.log(chalk.yellow(`warning: 该文件 "${filePath}" 没有按照约定命名，将忽略生成相应数据。`))
-           return
-         }
-         if (type === 'md') { // 过滤非md文件
-           // console.log('filePath', filePath);
-           filePath = filePath.replace('docs/', '');
-           filesList.push([filePath, name]);
-           // filesList.push({
-           //   name,
-           //   filePath
-           // });
-         }
- 
-       }
-     }
-   });
-   return filesList;
- }
- 
- module.exports = readFileList;
+          return
+        }
+        if (type === 'md') { // 过滤非md文件
+          // console.log('filePath', filePath);
+          filePath = filePath.replace('docs/', '');
+          filesList.push([filePath, name]);
+          // filesList.push({
+          //   name,
+          //   filePath
+          // });
+        }
+
+      }
+    }
+  });
+  return filesList;
+}
+
+module.exports = readFileList;
